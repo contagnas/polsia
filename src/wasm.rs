ae163fa7d88d3edbc0b18d9b9ace062e4b410198
Charlie Hills <contagnas@gmail.com>
2025-06-06 00:58:11 -0700

    Improve wasm error reporting

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,21 +1,112 @@
 use crate::{parser, unify_tree};
+use ariadne::{Color, Label, Report, ReportKind, sources};
 use chumsky::prelude::*;
 use wasm_bindgen::prelude::*;
 
+use crate::{SpannedValue, ValType, ValueKind, types::Span};
+
+fn find_unresolved(value: &SpannedValue) -> Option<(Span, ValType)> {
+    match &value.kind {
+        ValueKind::Type(t) => Some((value.span, t.clone())),
+        ValueKind::Array(items) => {
+            for item in items {
+                if let Some(res) = find_unresolved(item) {
+                    return Some(res);
+                }
+            }
+            None
+        }
+        ValueKind::Object(members) => {
+            for (_, v, _) in members {
+                if let Some(res) = find_unresolved(v) {
+                    return Some(res);
+                }
+            }
+            None
+        }
+        _ => None,
+    }
+}
+
 #[wasm_bindgen]
 pub fn polsia_to_json(src: &str) -> Result<String, String> {
-    match parser().parse(src).into_result() {
+    let filename = "input".to_string();
+    let parse_result = parser().parse(src).into_result();
+    match parse_result {
         Ok(ast) => match unify_tree(&ast) {
-            Ok(v) => Ok(v.to_value().to_pretty_string()),
-            Err(err) => Err(err.msg),
+            Ok(value) => {
+                if let Some((span, t)) = find_unresolved(&value) {
+                    let msg = format!("value of type {:?} is unspecified", t);
+                    let span_range = span.into_range();
+                    let mut buf = Vec::new();
+                    Report::build(ReportKind::Error, (filename.clone(), span_range.clone()))
+                        .with_message(&msg)
+                        .with_label(
+                            Label::new((filename.clone(), span_range))
+                                .with_message(&msg)
+                                .with_color(Color::Red),
+                        )
+                        .finish()
+                        .write(sources([(filename.clone(), src)]), &mut buf)
+                        .unwrap();
+                    Err(String::from_utf8(buf).unwrap())
+                } else {
+                    Ok(value.to_value().to_pretty_string())
+                }
+            }
+            Err(err) => {
+                use chumsky::error::LabelError;
+                let mut e = Rich::custom(err.span, err.msg.clone());
+                <Rich<_> as LabelError<&str, _>>::in_context(
+                    &mut e,
+                    "previous value here",
+                    err.prev_span,
+                );
+                let span = (*e.span()).into_range();
+                let msg = e.to_string();
+                let mut buf = Vec::new();
+                Report::build(ReportKind::Error, (filename.clone(), span.clone()))
+                    .with_message(&msg)
+                    .with_label(
+                        Label::new((filename.clone(), span.clone()))
+                            .with_message(&msg)
+                            .with_color(Color::Red),
+                    )
+                    .with_labels(e.contexts().map(|(label, span)| {
+                        Label::new((filename.clone(), span.into_range()))
+                            .with_message(label.to_string())
+                            .with_color(Color::Yellow)
+                    }))
+                    .finish()
+                    .write(sources([(filename.clone(), src)]), &mut buf)
+                    .unwrap();
+                Err(String::from_utf8(buf).unwrap())
+            }
         },
         Err(errs) => {
-            let msg = errs
-                .into_iter()
-                .map(|e| e.to_string())
-                .collect::<Vec<_>>()
-                .join("\n");
-            Err(msg)
+            let mut out = String::new();
+            for e in errs {
+                let span = (*e.span()).into_range();
+                let msg = e.to_string();
+                let mut buf = Vec::new();
+                Report::build(ReportKind::Error, (filename.clone(), span.clone()))
+                    .with_message(&msg)
+                    .with_label(
+                        Label::new((filename.clone(), span.clone()))
+                            .with_message(&msg)
+                            .with_color(Color::Red),
+                    )
+                    .with_labels(e.contexts().map(|(label, span)| {
+                        Label::new((filename.clone(), span.into_range()))
+                            .with_message(label.to_string())
+                            .with_color(Color::Yellow)
+                    }))
+                    .finish()
+                    .write(sources([(filename.clone(), src)]), &mut buf)
+                    .unwrap();
+                out.push_str(&String::from_utf8(buf).unwrap());
+            }
+            Err(out)
         }
     }
 }
